ef958411a51e1410cb4467663ec46cf77bc14b03
Serhii Bublyk <serhiibublyk88@gmail.com>
2025-07-07 23:13:26 +0200

    initial frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  darkMode: 'class',
+  content: [path.join(__dirname, 'index.html'), path.join(__dirname, 'src/**/*.{js,ts,jsx,tsx}')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
